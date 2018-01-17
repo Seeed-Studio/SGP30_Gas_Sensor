38becab5a8eb4435c6da234c00ca6d2a4770a7db
linux-downey <1148256554@qq.com>
2018-01-17 19:50:48 +0800

    Arduino project file for base function

---
+++ examps/base_example/base_example.ino
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+
+#include "sensirion_common.h"
+#include "sgp30.h"
+
+
+void setup() {
+    s16 err;
+    u16 scaled_ethanol_signal, scaled_h2_signal;
+  Serial.begin(115200);
+  Serial.println("serial start!!");
+  /*Init module,Reset all baseline,The initialization takes up to around 15 seconds, during which
+all APIs measuring IAQ(Indoor air quality ) output will not change.Default value is 400(ppm) for co2,0(ppb) for tvoc*/
+  while (sgp_probe() != STATUS_OK) {
+         Serial.println("SGP failed");
+         while(1);
+    }
+    /*Read H2 and Ethanol signal in the way of blocking*/
+    err = sgp_measure_signals_blocking_read(&scaled_ethanol_signal,
+                                            &scaled_h2_signal);
+    if (err == STATUS_OK) {
+        Serial.println("get ram signal!");
+    } else {
+         Serial.println("error reading signals"); 
+    }
+     err = sgp_iaq_init();
+     //
+}
+
+void loop() {
+  s16 err=0;
+  u16 tvoc_ppb, co2_eq_ppm;
+  err = sgp_measure_iaq_blocking_read(&tvoc_ppb, &co2_eq_ppm);
+        if (err == STATUS_OK) {
+             Serial.print("tVOC  Concentration:");
+             Serial.print(tvoc_ppb);
+             Serial.println("ppb");
+             
+              Serial.print("CO2eq Concentration:");
+              Serial.print(co2_eq_ppm);
+               Serial.println("ppm");
+        } else {
+             Serial.println("error reading IAQ values\n"); 
+        }
+  delay(1000);
+}
